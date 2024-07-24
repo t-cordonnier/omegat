da1cfbf1eb2538035160f62510eed322d8f621a6
Thomas CORDONNIER <t_cordonnier@yahoo.fr>
2024-07-24 18:03:44 +0200

    Test enforce

--- test/src/org/omegat/core/data/AutoTmxTest.java
+++ test/src/org/omegat/core/data/AutoTmxTest.java
@@ -26,10 +26,13 @@ package org.omegat.core.data;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Properties;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -96,7 +99,7 @@ public class AutoTmxTest {
     }
 
     @Test
-    public void testEnforce() throws Exception {
+    public void testEnforce1() throws Exception {
         ProjectProperties props = new ProjectProperties();
         props.setSourceLanguage("en");
         props.setTargetLanguage("fr");
@@ -127,7 +130,7 @@ public class AutoTmxTest {
         p.appendFromAutoTMX(enforceTMX, true);
         checkTranslation(ste, "bizbaz", TMXEntry.ExternalLinked.xENFORCED);
     }
-
+    
     SourceTextEntry createSTE(String id, String source) {
         EntryKey ek = new EntryKey("file", source, id, null, null, null);
         return new SourceTextEntry(ek, 0, null, null, new ArrayList<ProtectedPart>());
@@ -144,4 +147,70 @@ public class AutoTmxTest {
         assertEquals(expectedTranslation, e.translation);
         assertEquals(expectedExternalLinked, e.linked);
     }
+    
+    @Test
+    public void testEnforce2() throws Exception {
+        ProjectProperties props = new ProjectProperties();
+        props.setSourceLanguage("en");
+        props.setTargetLanguage("fr");
+        props.setTargetTokenizer(LuceneFrenchTokenizer.class);
+        File file = new File("test/data/autotmx/enforce-test2/enforce.tmx");
+        ExternalTMX enforceTMX = new ExternalTMFactory.TMXLoader(file)
+                .setDoSegmenting(props.isSentenceSegmentingEnabled())
+                .load(props.getSourceLanguage(), props.getTargetLanguage());
+
+        Core.initializeConsole(new HashMap<String, String>());
+
+        p = new RealProject(props);
+        p.projectTMX = new ProjectTMX(props.getSourceLanguage(), props.getTargetLanguage(), false,
+                new File("test/data/autotmx/enforce-test2/project_save.tmx"), new ProjectTMX.CheckOrphanedCallback() {
+                    public boolean existSourceInProject(String src) {
+                        return true;
+                    }
+
+                    public boolean existEntryInProject(EntryKey key) {
+                        return true;
+                    }
+                });
+        HashMap<String,SourceTextEntry> entriesMap = new HashMap<>();
+        try (java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(
+            new java.io.FileInputStream("test/data/autotmx/enforce-test2/source-file.properties")))) {
+            String line; SourceTextEntry ste;
+            while ((line = reader.readLine()) != null) {
+                if (! line.contains("=")) continue;                
+                String key = line.substring(0, line.indexOf("=")), val = line.substring(line.indexOf("=") + 1);
+                p.allProjectEntries.add(ste = createSTE("source-file.properties", key, val));
+                entriesMap.put(key, ste);
+            }
+        }
+        p.importHandler = new ImportFromAutoTMX(p, p.allProjectEntries);
+        p.appendFromAutoTMX(enforceTMX, true);
+        TMXEntry e = p.getTranslationInfo(entriesMap.get("tu_0_A"));
+        assertFalse(e.isTranslated());
+        e = p.getTranslationInfo(entriesMap.get("tu_0_B"));
+        assertFalse(e.isTranslated());
+        checkTranslation(entriesMap.get("tu_1_A"), "Traduction par défaut dans enforce", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_1_B"), "Traduction par défaut dans enforce", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_1_C"), "Traduction par défaut dans enforce", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_2_A"), "Traduction alternative dans enforce (première instance uniquement)", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_2_B"), "Traduction par défaut dans enforce (pour les autres)", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_2_C"), "Traduction par défaut dans enforce (pour les autres)", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_3_A"), "Traduction par défaut dans enforce propagée partout", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_3_B"), "Traduction par défaut dans enforce propagée partout", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_3_C"), "Traduction par défaut dans enforce propagée partout", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_4_A"), "Traduction écrasante", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_4_B"), "Traduction par défaut dans la mémoire (écrasée une fois)", null);
+        checkTranslation(entriesMap.get("tu_4_C"), "Traduction par défaut dans la mémoire (écrasée une fois)", null);
+        checkTranslation(entriesMap.get("tu_5_A"), "Traduction par défaut dans enforce", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_5_B"), "Traduction alternative dans la mémoire", null); // not erased by enforce default
+        checkTranslation(entriesMap.get("tu_5_C"), "Traduction par défaut dans enforce", TMXEntry.ExternalLinked.xENFORCED);
+        checkTranslation(entriesMap.get("tu_6_A"), "Traduction par défaut dans la mémoire", null);
+        checkTranslation(entriesMap.get("tu_6_B"), "Traduction alternative dans enforce", TMXEntry.ExternalLinked.xENFORCED); // not erased by enforce default
+        checkTranslation(entriesMap.get("tu_6_C"), "Traduction par défaut dans la mémoire", null);
+    }
+    
+    SourceTextEntry createSTE(String file, String id, String source) {
+        EntryKey ek = new EntryKey(file, source, id, null, null, null);
+        return new SourceTextEntry(ek, 0, null, null, new ArrayList<ProtectedPart>());
+    }    
 }
